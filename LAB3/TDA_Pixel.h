b64caa851932e44a709cacff91b888fe192e35ff
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2023-10-03 21:51:11 -0300

    se hacen cambios en los archivos

--- LAB3/TDA_Pixel.h
+++ LAB3/TDA_Pixel.h
@@ -1,5 +1,5 @@
-#ifndef TDA_Pixel_h_
-#define TDA_Pixel_h_
+#ifndef TDA_Pixel_h
+#define TDA_Pixel_h
 
 #include <stdio.h>
 
@@ -10,9 +10,39 @@ typedef struct TDA_Pixel{
 
 }pixel;
 
-pixel crearPixel(int coordX, int coordY, int color);
-void cambiarColorPixel(pixel* nuevo_pixel, int nuevoColor);
-void trasladarHorizontalmentePixel(pixel* nuevo_pixel, int desplazamientoX);
-void visualizarPIxel(pixel nuevo_pixel);
+//Dom: coordenada x del nuevo pixel - coordenada y del nuevo pixel - color del nuevo pixel
+//Rec: pixel con las corrdenadas y el color asignado
+//Descripcion: Crea un nuevo pixel con los datos dados
+pixel crearPixel(int coordX, int coordY, int color){
+
+    pixel nuevo_pixel;
+
+    nuevo_pixel.posy_x = coordX;
+    nuevo_pixel.pos_y = coordY;
+    nuevo_pixel.color = color;
+
+    return nuevo_pixel;
+}
+
+//Dom: pixel a cambiar - nuevo color del pixel
+//Rec: void
+//Descripcion: Cambia el color del pixel dado
+void cambiarColorPixel(pixel* nuevo_pixel, int nuevoColor){
+    nuevo_pixel->color = nuevoColor;
+}
+
+//Dom: pixel a cambiar - numero de desplazamientos
+//Rec: void
+//Descripcion: Traslada una cantidad de posiciones en la coordenada x al pixel
+void trasladarHorizontalmentePixel(pixel* nuevo_pixel, int desplazamientoX){
+    nuevo_pixel->posy_x = nuevo_pixel->posy_x + desplazamientoX;
+}
+
+//Dom: pixel a mostrar
+//Rec: void
+//Descripcion: Muestra en pantalla la coordenada x e y del pixel, ademas del color
+void visualizarPIxel(pixel nuevo_pixel){
+    printf("posicion x: %d, posicion y: %d, color: %d", nuevo_pixel.posy_x, nuevo_pixel.pos_y, nuevo_pixel.color);
+}
 
 #endif
--- S3_ref/Ejercicio_1.c
+++ S3_ref/Ejercicio_1.c
@@ -2,7 +2,33 @@
 
 // calcular moda
 int moda(int* conjunto, int size){
+    int moda = conjunto[0]; 
+    int frecuenciaMax = 1;
 
+    int elementoActual = conjunto[0]; 
+    int frecuenciaActual = 1;
+
+    for (int i = 1; i < size; ++i) {
+        if (conjunto[i] == elementoActual) {
+
+            ++frecuenciaActual;
+        } else {
+           
+            if (frecuenciaActual > frecuenciaMax) {
+                moda = elementoActual;
+                frecuenciaMax = frecuenciaActual;
+            }
+
+            elementoActual = conjunto[i];
+            frecuenciaActual = 1;
+        }
+    }
+
+    if (frecuenciaActual > frecuenciaMax) {
+        moda = elementoActual;
+    }
+
+    return moda;
 }
 
 int main(){
@@ -11,6 +37,8 @@ int main(){
     int size = 6;
 
     int result = moda(conjunto, 6);
+
+    printf("%d\n", result);
 
     return 0;
 }
